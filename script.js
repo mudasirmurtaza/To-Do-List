9d44fcba8566718547859aa1c653f3b7ad685091
Mudasir Murtaza <109586296+mudasirmurtaza@users.noreply.github.com>
2024-10-10 17:08:33 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,55 @@
 var input = document.querySelector(".entered-list");
 var addBtn = document.querySelector(".add-list");
 var tasks = document.querySelector(".list");
-input.addEventListener("keyup", ()  => {
-    if(input.value.trim() != 0) {
+
+input.addEventListener("keyup", () => {
+    if (input.value.trim() != 0) {
         addBtn.classList.add("active");
-    }
-    else {
+    } else {
         addBtn.classList.remove("active");
     }
-})
-// add new item to the list
+});
+
+// Add new item to the list
 addBtn.addEventListener('click', () => {
-    if(input.value.trim() != 0) {
+    if (input.value.trim() != 0) {
         let newItem = document.createElement('div');
         newItem.classList.add('item');
         newItem.innerHTML = `
         <p>${input.value}</p>
         <div class="item-btn">
             <i class="fa-solid fa-xmark"></i>
-            </div>
-            `
-            tasks.appendChild(newItem);
-            input.value = '';
-    }
-    else {
+        </div>
+        `;
+        tasks.appendChild(newItem);
+        input.value = '';
+        addBtn.classList.remove("active");
+    } else {
         alert("Please enter a task");
     }
-})
-// delete items from list
+});
+
+// Delete items from list
 tasks.addEventListener('click', (e) => {
-    if(e.target.classList.contains('fa-xmark')) {
+    if (e.target.classList.contains('fa-xmark')) {
         e.target.parentElement.parentElement.remove();
     }
-})
+});
+
+// Add task using Enter key
 document.addEventListener('keypress', (e) => {
-    if(e.key === 'Enter') {
-        if(input.value.trim() != 0) {
-            let newItem = document.createElement('div');
-            newItem.classList.add('item');
-            newItem.innerHTML = `
-            <p>${input.value}</p>
-            <div class="item-btn">
-                <i class="fa-solid fa-xmark"></i>
-            </div>
-            `
-            tasks.appendChild(newItem);
-            input.value = '';
+    if (e.key === 'Enter' && addBtn.classList.contains('active')) {
+        let newItem = document.createElement('div');
+        newItem.classList.add('item');
+        newItem.innerHTML = `
+        <p>${input.value}</p>
+        <div class="item-btn">
+            <i class="fa-solid fa-xmark"></i>
+        </div>
+        `;
+        tasks.appendChild(newItem);
+        input.value = '';
+        addBtn.classList.remove("active");
     }
-    else {
-        alert("Please enter a task");
-    }
- }
-})
+});
+
